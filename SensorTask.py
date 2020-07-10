0ec83a692ea385926f1c8015b095498d953f9433
lugi55 <pi@pi9.aut.ei.htwg-konstanz.de>
2020-07-11 00:29:19 +0200

    Sensor Task

--- SensorTask.py
+++ SensorTask.py
@@ -1,3 +1,6 @@
+from IMU.lis3mdl import LIS3MDL
+from IMU.lps25h import LPS25H
+from IMU.lsm6ds33 import LSM6DS33
 from vcgencmd import Vcgencmd
 import paho.mqtt.client as paho
 import logging
@@ -6,6 +9,7 @@ import time
 import json
 import signal
 import sys
+import random
 
 def main():
 	#init logging module
@@ -16,25 +20,134 @@ def main():
 	niceValue = os.nice(10)
 	logging.info('SensorTask\t\tniceValue:%s',niceValue)
 
-	#init MQTT Client
-	client = paho.Client()
-	client.connect(host='localhost',port=1883)
-	client.loop_start()
-	vcgm = Vcgencmd()
-	next_call = time.time()
-
 	#register signalHandler
 	signal.signal(signal.SIGINT, signalHandler)
 
+	mySensor = Sensor()
+	mySensor.setLogin(True)
+	mySensor.run()
+
+class Sensor():
+	def __init__(self):
+		#login or not login
+		self.login = False
+
+		#Get IMU
+		self.lis3mdl = LIS3MDL()
+		self.lis3mdl.enable()
+
+		self.lps25h = LPS25H()
+		self.lps25h.enable()
+
+		self.lsm6ds33 = LSM6DS33()
+		self.lsm6ds33.enable()
+
+		#init MQTT Client
+		self.client = paho.Client()
+		self.client.connect(host='localhost',port=1883)
+		self.client.loop_start()
+
+		self.vcgm = Vcgencmd()
+		self.next_call = time.time()
+
+		self.humidity = 40
+		self.speed = 0
+		self.steeringAngle = 0
+		self.LIDAR = 10
+
+	def setLogin(self,boolValue):
+		self.login = boolValue
+
+	def randomWalk(self,start,stop,dx,x):
+		if not self.login: dx = dx*10
+		if x<start:
+			return x + random.uniform(-dx,0)
+		if x>stop:
+			return x + random.uniform(0,dx)
+		else:
+			return x + random.uniform(-1,1)
+
+	def puplishAll(self):
+		#dummy dict
+		self.dict = {"SensorValue1":[],"SensorValue3":[]}
+		#Temperature
+		self.temp = self.vcgm.measure_temp()
+		self.tempDict = {"name":"Temperature","timestamp":time.time(),"value":self.temp}
+		#LIDAR
+		self.LIDAR += self.randomWalk(start=5,stop=150,dx=0.5,x=self.LIDAR)
+		self.LIDARDict = {"name":"LIDAR","timestamp":time.time(),"value":self.LIDAR}
+		#Speed
+		self.speed += self.randomWalk(start=0,stop=150,dx=0.5,x=self.speed)
+		self.speedDict = {"name":"Speed","timestamp":time.time(),"value":self.speed}
+		#SteeringAngle
+		self.steeringAngle += self.randomWalk(start=5,stop=150,dx=0.5,x=self.steeringAngle)
+		self.steeringAngleDict = {"name":"LIDAR","timestamp":time.time(),"value":self.steeringAngle}
+		#Altimeter
+		self.altimeter = self.lps25h.get_barometer_raw()
+		self.altimeterDict = {"name":"Altimeter","timestamp":time.time(),"value":self.altimeter}
+		#Humidity
+		self.humidity = self.randomWalk(start=10,stop=95,dx=0.1,x=self.humidity)
+		self.humidityDict = {"name":"Humidity","timestamp":time.time(),"value":self.humidity}
+		#Acceleration
+		self.accel = self.lsm6ds33.get_accelerometer_g_forces()
+		self.accelDict = {"name":"Acceleration","timestamp":time.time(),"valueX":self.accel[0],"valueY":self.accel[1],"valueZ":self.accel[2]}
+		#Gyro
+		self.gyro = self.lsm6ds33.get_gyro_angular_velocity()
+		self.gyroDict = {"name":"Gyro","timestamp":time.time(),"valueX":self.gyro[0],"valueY":self.gyro[1],"valueZ":self.gyro[2]}
+		#Magnetometer
+		self.mag = self.lis3mdl.get_magnetometer_raw()
+		self.magDict = {"name":"Magnetometer","timestmap":time.time(),"valueX":self.mag[0],"valueY":self.mag[1],"valueZ":self.mag[2]}
+		#make big dict to publish
+		self.dict["SensorValue1"].append(self.humidityDict)
+		self.dict["SensorValue1"].append(self.tempDict)
+		self.dict["SensorValue1"].append(self.LIDARDict)
+		self.dict["SensorValue1"].append(self.speedDict)
+		self.dict["SensorValue1"].append(self.steeringAngleDict)
+		self.dict["SensorValue1"].append(self.altimeterDict)
+		self.dict["SensorValue3"].append(self.accelDict)
+		self.dict["SensorValue3"].append(self.gyroDict)
+		self.dict["SensorValue3"].append(self.magDict)
+		#publish to local Brocker
+		self.client.publish('SensorTask', json.dumps(self.dict), qos = 0)
+		#when not login 1s measurement frequency
+		self.next_call = self.next_call+0.1
+		time.sleep(self.next_call - time.time())
+
+	def puplishPart(self):
+		#dummy dict
+		self.dict = {"SensorValue1":[],"SensorValue3":[]}
+		#Temperature
+		self.temp = self.vcgm.measure_temp()
+		self.tempDict = {"name":"Temperature","timestamp":time.time(),"value":self.temp}
+		#Humidity
+		self.humidity = self.randomWalk(start=10,stop=95,dx=0.1,x=self.humidity)
+		self.humidityDict = {"name":"Humidity","timestamp":time.time(),"value":self.humidity}
+		#Acceleration
+		self.Accel = self.lsm6ds33.get_accelerometer_g_forces()
+		self.AccelDict = {"name":"Acceleration","timestamp":time.time(),"valueX":self.Accel[0],"valueY":self.Accel[1],"valueZ":self.Accel[2]}
+		#Gyro
+		self.Gyro = self.lsm6ds33.get_gyro_angular_velocity()
+		self.GyroDict = {"name":"Gyro","timestamp":time.time(),"valueX":self.Gyro[0],"valueY":self.Gyro[1],"valueZ":self.Gyro[2]}
+		#make big dict to publish
+		self.dict["SensorValue1"].append(self.humidityDict)
+		self.dict["SensorValue1"].append(self.tempDict)
+		self.dict["SensorValue3"].append(self.AccelDict)
+		self.dict["SensorValue3"].append(self.GyroDict)
+		#publish to local Brocker
+		self.client.publish('SensorTask', json.dumps(self.dict), qos = 0)
+		#when not login 1s measurement frequency
+		self.next_call = self.next_call+1
+		time.sleep(self.next_call - time.time())
+
 	#measurement Loop
-	while True:
-		temperature = vcgm.measure_temp()
-		temperature = str(temperature)
-		timestamp = str(time.time())
-		dict = {'timestamp':timestamp,'temperature':temperature}
-		client.publish('SensorTask/Temperature', json.dumps(dict), qos = 0)
-		next_call = next_call+0.1
-		time.sleep(next_call - time.time())
+	def run(self):
+		while True:
+			if self.login:
+				self.puplishAll()
+			else:
+				self.puplishPart()
+
+
 
 
 #signal Handler (Ctrl+C)
